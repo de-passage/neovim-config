9b94a6916c61e7fb8074cc1a348684e5b267d26c
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2024-05-13 17:34:55 +0200

    Add example to monkey-patch strings

---
+++ lua/utils/monkey.lua
@@ -0,0 +1,19 @@
+---@param str string
+---@param match string
+---@return string[]
+function string.split(str, match)
+  local eol_char = 1
+  local start = 1
+  local return_value = {}
+
+  while start < #str do
+    eol_char, stop = str:find(match, start)
+    if eol_char == nil then
+      return_value[#return_value + 1] = str:sub(start, #str)
+      break
+    end
+    return_value[#return_value + 1] = str:sub(start, eol_char - 1)
+    start = stop + 1
+  end
+  return return_value
+end
