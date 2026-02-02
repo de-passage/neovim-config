d2a61b3569c46944e328ea9ef5ce1c3d50b4d414
Sylvain Leclercq <contact@sylvainleclercq.com>
2026-02-02 01:41:54 +0000

    Add basic file to load plugins with lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,152 +6,154 @@ local function setup(modulename)
   return 'require("' .. modulename .. '").setup()'
 end
 
-return require('packer').startup(function(use)
-  -- Packer needs to know itself...
-  use { "wbthomason/packer.nvim" }
-
+return {
+  -- Colorscheme
+  {
+    "EdenEast/nightfox.nvim",
+    config = configfile('nightfox')
+  },
   -- Essentials
-  use 'tpope/vim-surround'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-unimpaired'
-  use 'mbbill/undotree'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-tree/nvim-web-devicons'
-  use {
+  { 'tpope/vim-surround' },
+  { 'tpope/vim-fugitive' },
+  { 'tpope/vim-unimpaired' },
+  { 'mbbill/undotree' },
+  { 'nvim-lua/plenary.nvim' },
+  { 'nvim-tree/nvim-web-devicons' },
+  {
     'numToStr/Comment.nvim',
     config = setup('Comment')
-  }
+  },
 
-  use {
+  {
     'windwp/nvim-autopairs',
     config = setup('nvim-autopairs')
-  }
+  },
 
   -- Git
-  use {
+  {
     'lewis6991/gitsigns.nvim',
     config = setup('gitsigns')
-  }
+  },
 
   -- Syntax analysis
-  use {
+  {
     'nvim-treesitter/nvim-treesitter',
     run = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
     end,
     config = configfile('treesitter')
-  }
+  },
 
   -- Pretty bars
-  use {
+  {
     'nvim-lualine/lualine.nvim',
     wants = 'nvim-web-devicons',
     requires = {
       'nvim-tree/nvim-web-devicons', opt = true
     },
     config = setup('lualine')
-  }
+  },
 
   -- LSP configuration
-  use {
+  {
     'nvimtools/none-ls.nvim',
     requires = { { 'nvim-lua/plenary.nvim' } },
     config = configfile("null_ls")
-  }
-  use {
+  },
+  {
     'neovim/nvim-lspconfig',
     config = configfile('lspconfig')
-  }
-  use {
+  },
+  {
     'RRethy/vim-illuminate',
     config = function()
       local illum = require 'illuminate'
       vim.keymap.set('n', '<C-n>', illum.goto_next_reference)
       vim.keymap.set('n', '<C-p>', illum.goto_prev_reference)
     end,
-  }
-  use {
+  },
+  {
     'folke/neodev.nvim'
-  }
+  },
 
-  use {
+  {
     "hedyhli/outline.nvim",
     config = function()
       vim.keymap.set("n", "<leader>v", "<cmd>Outline<cr>")
       require("outline").setup {
       }
     end
-  }
+  },
 
   -- Completion
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-vsnip'
-  use {
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-cmdline' },
+  { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+  { 'hrsh7th/cmp-vsnip' },
+  {
     'hrsh7th/nvim-cmp',
     config = configfile('cmp')
-  }
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-vsnip'
+  },
+  { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+  { 'hrsh7th/cmp-vsnip' },
 
   -- Snippets
-  use 'hrsh7th/vim-vsnip'
+  { 'hrsh7th/vim-vsnip' },
 
   -- Telescope (extension config is done in the config.telescope module)
-  use {
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.4',
     requires = { { 'nvim-lua/plenary.nvim' } },
     config = configfile('telescope')
-  }
-  use { 'nvim-telescope/telescope-ui-select.nvim' }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use {
+  },
+  { 'nvim-telescope/telescope-ui-select.nvim' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+  {
     "de-passage/telescope-makefile",
     requires = {
       { "akinsho/toggleterm.nvim" },
       { 'nvim-telescope/telescope.nvim' }
     },
-  }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
+  },
+  { "nvim-telescope/telescope-file-browser.nvim" },
 
   -- Terminal
-  use {
+  {
     "akinsho/toggleterm.nvim",
     tag = '*',
     config = configfile('toggleterm')
-  }
+  },
 
-  use {
+  {
     'github/copilot.vim',
     config = configfile('copilot')
-  }
+  },
 
-  use {
+  {
     "williamboman/mason.nvim",
     run = ":MasonUpdate",
     config = configfile('mason')
-  }
+  },
 
-  use 'purescript-contrib/purescript-vim'
+  { 'purescript-contrib/purescript-vim' },
 
   -- Debugger support
-  use {
+  {
     'mfussenegger/nvim-dap',
     config = configfile('dap')
-  }
-  use {
+  },
+  {
     'theHamsta/nvim-dap-virtual-text',
     requires = {
       { "mfussenegger/nvim-dap" },
     },
-  }
+  },
 
-  use {
+  {
     'rcarriga/nvim-dap-ui',
     requires = {
       {
@@ -161,27 +163,21 @@ return require('packer').startup(function(use)
         'nvim-neotest/nvim-nio',
       }
     },
-  }
+  },
 
-  use {
+  {
     "leoluz/nvim-dap-go",
     config = setup('dap-go'),
     requires = {
       { "mfussenegger/nvim-dap" },
     },
-  }
+  },
 
-  use {
+  {
     'RaafatTurki/hex.nvim',
     config = setup('hex')
-  }
+  },
+  { "Shirk/vim-gas" },
+  { "HiPhish/info.vim" },
 
-  -- Colorscheme
-  use {
-    "EdenEast/nightfox.nvim",
-    config = configfile('nightfox')
-  }
-  use "Shirk/vim-gas"
-  use "HiPhish/info.vim"
-
-end)
+}
